f78337d308556633c2e431c4e874825a7f2bf3f0
Manuel Bachl <manuel@coding-nomad.com>
2023-03-18 21:28:06 +0800

    feat(random): added getRandomInteger helper

---
+++ src/helpers/random/interfaces/MinMaxNumberConfig.ts
@@ -0,0 +1,17 @@
+export interface MinMaxNumberConfig {
+  /**
+   * @description
+   * min value
+   *
+   * @default 0
+   */
+  min?: number;
+
+  /**
+   * @description
+   * max value
+   *
+   * @default 100
+   */
+  max?: number;
+}
