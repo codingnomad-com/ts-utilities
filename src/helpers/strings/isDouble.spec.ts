94f04048fd4eede84271ca7c59e1bd4815e2ac36
Manuel Bachl <manuel@coding-nomad.com>
2023-03-18 20:48:31 +0800

    feat(strings): added isDouble check

--- src/helpers/strings/isDouble.spec.ts
+++ src/helpers/strings/isDouble.spec.ts
@@ -1,54 +1,86 @@
 import { expect, test } from '@playwright/test';
-import { removeNonNumeric } from './removeNonNumeric';
+import { isDouble } from './isDouble';
 
 test.describe.configure({ mode: 'parallel' });
 
-test('returns empty string if value is null', async () => {
-  const formattedValue = removeNonNumeric(null);
+test('returns false string if value is null', async () => {
+  const formattedValue = isDouble(null);
 
-  expect(formattedValue).toBe('');
+  expect(formattedValue).toBeFalsy();
 });
 
 test('returns empty string if value is undefined', async () => {
-  const formattedValue = removeNonNumeric();
+  const formattedValue = isDouble();
 
-  expect(formattedValue).toBe('');
+  expect(formattedValue).toBeFalsy();
 });
 
 test('returns empty string if value is empty', async () => {
-  const formattedValue = removeNonNumeric('');
+  const formattedValue = isDouble('');
 
-  expect(formattedValue).toBe('');
+  expect(formattedValue).toBeFalsy();
 });
 
-test('returns empty string if value does not contain any numbers', async () => {
+test('returns false if value does contain whitespace', async () => {
+  const input = '     ';
+
+  const formattedValue = isDouble(input);
+
+  expect(formattedValue).toBeFalsy();
+});
+
+test('returns false if value does not contain any numbers', async () => {
   const input = 'asdfasdfASKJDKLASJD"!@@@##%*()_+=-{[}^~]?/:.>,<';
 
-  const formattedValue = removeNonNumeric(input);
+  const formattedValue = isDouble(input);
 
-  expect(formattedValue).toBe('');
+  expect(formattedValue).toBeFalsy();
 });
 
-test('returns input string if value contains only numbers', async () => {
-  const input = '1234567890';
+test('returns false if value has whitespace around numbers', async () => {
+  const input = ' 123 ';
 
-  const formattedValue = removeNonNumeric(input);
+  const formattedValue = isDouble(input);
 
-  expect(formattedValue).toBe(input);
+  expect(formattedValue).toBeFalsy();
 });
 
-test('returns only numeric characters', async () => {
-  const input = 'oiruge9p47tyuHDED341P(*&WGHEDfp9)I#$%*R';
+test('returns true if value contains . (dot)', async () => {
+  const input = '12.3';
 
-  const formattedValue = removeNonNumeric(input);
+  const formattedValue = isDouble(input);
 
-  expect(formattedValue).toBe('9473419');
+  expect(formattedValue).toBeTruthy();
 });
 
-test('returns string with 0 (zero) if string with 0 (zero) was received', async () => {
-  const input = '0';
+test('returns false if value contains more than one . (dot)', async () => {
+  const input = '12.3.456';
 
-  const formattedValue = removeNonNumeric(input);
+  const formattedValue = isDouble(input);
 
-  expect(formattedValue).toBe(input);
+  expect(formattedValue).toBeFalsy();
+});
+
+test('returns false if value contains , (comma)', async () => {
+  const input = '12,3';
+
+  const formattedValue = isDouble(input);
+
+  expect(formattedValue).toBeFalsy();
+});
+
+test('returns true if value contains only numeric characters', async () => {
+  const input = '123';
+
+  const formattedValue = isDouble(input);
+
+  expect(formattedValue).toBeTruthy();
+});
+
+test('returns true if value contains only numeric characters, even if many', async () => {
+  const input = '123456789012345678901234567890';
+
+  const formattedValue = isDouble(input);
+
+  expect(formattedValue).toBeTruthy();
 });
